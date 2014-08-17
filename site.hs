021a5f0d922a7218f9f10d2c2cb7e039a346a0a0
Friedrich von Never <fvnever@gmail.com>
2014-08-17 20:48:07 +0700

    Update index page.

--- site.hs
+++ site.hs
@@ -65,7 +65,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "int 20h"             `mappend`
                     defaultContext
 
             getResourceBody
