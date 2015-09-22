7709adc5bbeb4ad620df9d869983ec8d44495efe
Friedrich von Never <friedrich@fornever.me>
2015-09-22 23:13:54 +0600

    Layout and packages.

--- EvilPlanner/Controllers/MainController.fs
+++ EvilPlanner/Controllers/MainController.fs
@@ -6,8 +6,11 @@ open System.Linq
 open System.Web
 open System.Web.Mvc
 open System.Web.Mvc.Ajax
+open EvilPlanner.Data
 
 type HomeController() =
     inherit Controller()
-    member this.Index () = this.View()
+    member this.Index () = 
+        use context = new EvilPlannerContext()
+        this.View("Quotation")
 
--- EvilPlanner/Global.asax.fs
+++ EvilPlanner/Global.asax.fs
@@ -10,19 +10,9 @@ open System.Web.Optimization
 
 type BundleConfig() =
     static member RegisterBundles (bundles:BundleCollection) =
-        bundles.Add(ScriptBundle("~/bundles/jquery").Include([|"~/Scripts/jquery-{version}.js"|]))
-
-        // Use the development version of Modernizr to develop with and learn from. Then, when you're
-        // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-        bundles.Add(ScriptBundle("~/bundles/modernizr").Include([|"~/Scripts/modernizr-*"|]))
-
-        bundles.Add(ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/respond.js"))
-
         bundles.Add(StyleBundle("~/Content/css").Include(
-                        "~/Content/bootstrap.css",
-                        "~/Content/site.css"))
+                        "~/Content/main.css",
+                        "~/Content/quotations.css"))
 
 /// Route for ASP.NET MVC applications
 type Route = { 
