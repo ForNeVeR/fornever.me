b8c0f49dab787ca948b7baeb2766709b959a45d3
Friedrich von Never <friedrich@fornever.me>
2015-10-25 21:33:38 +0600

    Add working Freya project template.

---
+++ ForneverMind/Common.fs
@@ -0,0 +1,20 @@
+﻿module ForneverMind.Common
+
+open System.Text
+
+open Arachne.Http
+open Freya.Core
+open Freya.Machine
+open Freya.Machine.Extensions.Http
+
+let private utf8 = Freya.init [ Charset.Utf8 ]
+let private json = Freya.init [ MediaType.Html ]
+
+let get = Freya.init [ GET ]
+
+let machine =
+    freyaMachine {
+        using http
+        charsetsSupported utf8
+        mediaTypesSupported json
+    }
---
+++ ForneverMind/Routes.fs
@@ -0,0 +1,43 @@
+﻿module ForneverMind.Routes
+
+open System
+open System.Globalization
+open System.Text
+
+open Arachne.Http
+open Arachne.Uri.Template
+
+open Freya.Core
+open Freya.Machine
+open Freya.Machine.Extensions.Http
+open Freya.Machine.Extensions.Http.Cors
+open Freya.Machine.Router
+open Freya.Router
+open Freya.Router.Lenses
+
+let private handleIndex _ =
+    freya {
+        return
+            {
+                Description =
+                    {
+                        Charset = Some Charset.Utf8
+                        Encodings = None
+                        MediaType = Some MediaType.Html
+                        Languages = None
+                    }
+                Data = "<h1>Hello World</h1>"B
+            }
+    }
+
+let private index =
+    freyaMachine {
+        including Common.machine
+        methodsSupported Common.get
+        handleOk handleIndex
+    } |> FreyaMachine.toPipeline
+
+let router =
+     freyaRouter {
+        resource (UriTemplate.Parse "/") index
+     } |> FreyaRouter.toPipeline
