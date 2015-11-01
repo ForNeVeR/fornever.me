bc70e36c400058397e280e4ebf56287cbf1aa7cc
Friedrich von Never <friedrich@fornever.me>
2015-11-01 22:11:01 +0600

    Serve static resources from the images subdirectory.

--- ForneverMind/Application.fs
+++ ForneverMind/Application.fs
@@ -2,10 +2,13 @@
 
 open Freya.Core
 open Microsoft.Owin
+open Owin
 
 type Application () =
-    member __.Configuration () =
-        OwinAppFunc.ofFreya Routes.router
+    member __.Configuration (app : IAppBuilder) =
+        app.UseStaticFiles "/images" |> ignore
+        let router = OwinAppFunc.ofFreya Routes.router
+        app.Run (fun c -> router.Invoke c.Environment)
 
 [<assembly: OwinStartup(typeof<Application>)>]
 ()
