5f57eb6a2193373040d3968f786f4a949a77d4b6
Friedrich von Never <friedrich@fornever.me>
2016-06-04 14:06:24 +0700

    Scripts: add script to create new posts

---
+++ Tools/New-Post.ps1
@@ -0,0 +1,26 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $FileName,
+
+    [Parameter(Mandatory = $true)]
+    [string] $Title,
+
+    [string] $Description = '',
+    [DateTime] $Date = (Get-Date),
+    [string] $ProjectPath = "$PSScriptRoot/../ForneverMind/ForneverMind.fsproj"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$dateFormatted = $Date.ToString('yyyy-MM-dd')
+$partialPath = "posts\$dateFormatted-$FileName.md"
+$path = "$PSScriptRoot/../ForneverMind/$partialPath"
+
+New-Item $path
+[xml]$project = Get-Content $ProjectPath
+$contentGroup = $project.Project.ItemGroup | ? { $_.Content }
+$latestPost = $contentGroup.ChildNodes | ? { $_.Include -and $_.Include.StartsWith('posts\') } | Select-Object -Last 1
+$element = $project.CreateElement('Content', $project.Project.NamespaceURI)
+$element.SetAttribute('Include', $partialPath)
+$contentGroup.InsertAfter($element, $latestPost)
+$project.Save($ProjectPath)
