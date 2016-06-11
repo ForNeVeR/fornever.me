08cf1955da8f0fbab312e8407ef24727c9f02a60
Friedrich von Never <friedrich@fornever.me>
2016-06-11 12:14:44 +0700

    Fonts: drop non-standard fonts used by site

--- ForneverMind/Application.fs
+++ ForneverMind/Application.fs
@@ -7,7 +7,6 @@ open Owin
 type Application () =
     member __.Configuration (app : IAppBuilder) =
         app.UseStaticFiles "/images" |> ignore
-        app.UseStaticFiles "/fonts" |> ignore
         let router = OwinAppFunc.ofFreya Routes.router
         app.Run (fun c -> router.Invoke c.Environment)
 
