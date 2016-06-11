fca560b920d96550ce939cda4211678faaf0ccfb
Friedrich von Never <friedrich@fornever.me>
2016-06-11 12:17:12 +0700

    Project: release 1.2.1

--- ForneverMind.Tests/AssemblyInfo.fs
+++ ForneverMind.Tests/AssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind.Tests")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2016")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("f66cd922-9216-401e-96de-36263df07726")>]
-[<assembly: AssemblyVersion("1.2.0.0")>]
+[<assembly: AssemblyVersion("1.2.1.0")>]
 
 ()
--- ForneverMind/AssemblyInfo.fs
+++ ForneverMind/AssemblyInfo.fs
@@ -5,9 +5,9 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2016")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("2cac3278-465e-432e-9434-09feb4a6d98d")>]
-[<assembly: AssemblyVersion("1.2.0.0")>]
+[<assembly: AssemblyVersion("1.2.1.0")>]
 
 ()
