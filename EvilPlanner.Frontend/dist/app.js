034e91d21c6d661ea6683fd25ab4fa1d5bd54bc2
Friedrich von Never <friedrich@fornever.me>
2016-07-03 15:09:24 +0700

    dist: drop auto-generated directory
    Close #45.

--- EvilPlanner.Frontend/dist/app.js
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var QuotationBlock = require('./quotation-block');
-ReactDOM.render(React.createElement(QuotationBlock, null), document.getElementById('content'));
