edecff7f846f4d4f6bd25f7dc333ed9f7522d50b
Friedrich von Never <friedrich@fornever.me>
2016-11-30 22:22:41 +0700

    #46: fix file paths

---
+++ EvilPlanner.Frontend/webpack.config.js
@@ -0,0 +1,16 @@
+﻿const path = require('path');
+
+const outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    resolve: { extensions: ['', '.css', '.js', '.html', '.ts', '.tsx'] },
+    entry: { 'app': './app' },
+    module: {
+        loaders: [
+            { test: /\.html$/, loader: 'file?name=[name].[ext]' },
+            { test: /\.css$/, loader: 'file?name=[name].[ext]' },
+            { test: /\.ts(x?)$/, loader: 'ts' }
+        ]
+    },
+    output: { path: outputPath, filename: '[name].js' }
+};
