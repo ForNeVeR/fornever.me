900a72749fc2c41298e366f597511c92c8d5bf98
Friedrich von Never <friedrich@fornever.me>
2017-02-12 16:58:14 +0700

    Project: update copyright year

--- ForneverMind.Tests/AssemblyInfo.fs
+++ ForneverMind.Tests/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind.Tests")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2016")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2017")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("f66cd922-9216-401e-96de-36263df07726")>]
 [<assembly: AssemblyVersion("1.5.1.0")>]
--- ForneverMind/AssemblyInfo.fs
+++ ForneverMind/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2016")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2017")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("2cac3278-465e-432e-9434-09feb4a6d98d")>]
 [<assembly: AssemblyVersion("1.5.1.0")>]
