fd9ea063c0b03de1df205070433af59e13d38f88
Friedrich von Never <friedrich@fornever.me>
2017-03-18 14:09:05 +0700

    Prepare-Talks: add new talk

--- Scripts/Prepare-Talks.ps1
+++ Scripts/Prepare-Talks.ps1
@@ -1,22 +1,35 @@
-param (
+﻿param (
     [string] $yarn = 'yarn',
     [string] $TargetPath = "$PSScriptRoot/../ForneverMind/talks"
 )
 
 $ErrorActionPreference = 'Stop'
 
-function modern-programming {
-    $output = "$TargetPath/modern-programming"
+function log($text, $header) {
+    if (-not $header) {
+        $header = 'Prepare-Talks.ps1'
+    }
+    Write-Output "[$header] $text"
+}
+
+function exec($command) {
+    log "$command $args" 'Prepare-Talks.exec'
+    & $command $args
+    if (!$?) {
+        throw "[build error] $command $args = $LASTEXITCODE"
+    }
+}
+
+function yarnInstall($name, $flags) {
+    $output = "$TargetPath/$name"
     Push-Location $output
     try {
-        Start-Process $yarn install, --ignore-engines -NoNewWindow -Wait
+        log "Installing $name"
+        exec $yarn install $flags
     } finally {
         Pop-Location
     }
 }
 
-if (!(Test-Path $TargetPath)) {
-    New-Item -ItemType Directory $TargetPath
-}
-
-modern-programming
+yarnInstall modern-programming --ignore-engines 
+yarnInstall net-core-slides
