8cf3dfdaaf0029758b050b9462b285b53bbee70e
Friedrich von Never <friedrich@fornever.me>
2017-03-30 21:28:38 +0700

    Prepare-Talks: force copy

--- Scripts/Prepare-Talks.ps1
+++ Scripts/Prepare-Talks.ps1
@@ -35,5 +35,4 @@ function yarnInstall($name, $flags) {
 yarnInstall modern-programming --ignore-engines 
 yarnInstall net-core-slides
 
-Remove-Item -Recurse $TargetPath/*
-Copy-Item -Recurse $RepoPath $TargetPath
+Copy-Item -Recurse -Force $RepoPath $TargetPath
