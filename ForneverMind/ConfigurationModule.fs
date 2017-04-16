20e7a12fc46c6cf82b357bb2990ad08d6f2e016e
Friedrich von Never <friedrich@fornever.me>
2017-04-16 15:22:06 +0700

    #43: remove BaseUrl from the ConfigurationModule

--- ForneverMind/ConfigurationModule.fs
+++ ForneverMind/ConfigurationModule.fs
@@ -5,13 +5,9 @@ open System.IO
 open Microsoft.AspNetCore.Hosting
 
 type ConfigurationModule  (app : IHostingEnvironment) =
-    // TODO[F]: Take this path from JSON configuration
-    let baseUrl = "http://localhost:5000/" //  ConfigurationManager.AppSettings.["BaseUrl"]
-
     let root = app.ContentRootPath
     let postsPath = Path.Combine(root, "posts")
     let viewsPath = Path.Combine(root, "views")
 
-    member __.BaseUrl = baseUrl
     member __.PostsPath = postsPath
     member __.ViewsPath = viewsPath
