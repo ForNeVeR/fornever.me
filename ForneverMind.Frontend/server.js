5fa58d38564f95a61cd3001d61e6b69ee9ace49f
Friedrich von Never <friedrich@fornever.me>
2017-05-28 23:24:45 +0700

    #58: fix style issues in server.js

--- ForneverMind.Frontend/server.js
+++ ForneverMind.Frontend/server.js
@@ -1,6 +1,6 @@
-var hljs = require('highlight.js');
+const hljs = require('highlight.js');
 module.exports = (callback, language, text) => {
-    let result = language && hljs.getLanguage(language)
+    const result = language && hljs.getLanguage(language)
         ? hljs.highlight(language, text).value
         : hljs.highlightAuto(text).value;
     callback(null, result);
