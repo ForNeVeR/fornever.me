0127ee15a4d368d24e0d7f45301d0946e163d729
Friedrich von Never <friedrich@fornever.me>
2017-11-18 21:18:21 +0700

    Project: release 3.0.1

--- ForneverMind/AssemblyInfo.fs
+++ ForneverMind/AssemblyInfo.fs
@@ -9,7 +9,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2017")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("2cac3278-465e-432e-9434-09feb4a6d98d")>]
-[<assembly: AssemblyVersion("3.0.0.0")>]
+[<assembly: AssemblyVersion("3.0.1.0")>]
 
 [<assembly: InternalsVisibleTo("ForneverMind.Tests")>]
 ()
