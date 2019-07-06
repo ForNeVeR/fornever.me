fca5f8ae0bb568a1ad0efe8d7b53fe7157fbcbf4
Friedrich von Never <friedrich@fornever.me>
2019-07-07 00:03:05 +0700

    Happy new year!

--- ForneverMind/AssemblyInfo.fs
+++ ForneverMind/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2018")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2019")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("2cac3278-465e-432e-9434-09feb4a6d98d")>]
 [<assembly: AssemblyVersion("3.1.0.0")>]
