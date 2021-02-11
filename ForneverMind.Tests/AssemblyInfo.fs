60da2bf68093b9213cecdbf1acc14bd9e039c795
Friedrich von Never <friedrich@fornever.me>
2021-02-12 00:58:18 +0700

    Legal: copyright year update

--- ForneverMind.Tests/AssemblyInfo.fs
+++ ForneverMind.Tests/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("ForneverMind.Tests")>]
 [<assembly: AssemblyProduct("ForneverMind")>]
-[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2020")>]
+[<assembly: AssemblyCopyright("Copyright © F. von Never 2015—2021")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("f66cd922-9216-401e-96de-36263df07726")>]
 [<assembly: AssemblyVersion("3.1.0.0")>]
