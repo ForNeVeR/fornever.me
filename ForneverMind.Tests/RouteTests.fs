905e7dce3770b2d131e3243d8e9ba23be2d2fd67
Friedrich von Never <friedrich@fornever.me>
2023-11-06 23:58:53 +0100

    (#115) Tests: add tests for 404

--- ForneverMind.Tests/RouteTests.fs
+++ ForneverMind.Tests/RouteTests.fs
@@ -1,6 +1,7 @@
 [<Xunit.Collection(ForneverMind.TestFramework.IntegrationTestUtil.IntegrationTests)>]
 module ForneverMind.Tests.RouteTests
 
+open System.Net
 open System.Threading.Tasks
 
 open Xunit
@@ -12,4 +13,20 @@ let ``Index page should resolve correctly``(): Task = withWebApp(fun client -> t
     let! result = client.GetAsync "/"
     Assert.Equal("/en/", result.RequestMessage.RequestUri.PathAndQuery)
     Assert.Equal("text/html", result.Content.Headers.ContentType.MediaType)
+})
+
+[<Fact>]
+let ``404 page should be resolved``(): Task = withWebApp(fun client -> task {
+    let doTest (url: string) message = task {
+        let! result = client.GetAsync url
+        let! content = result.Content.ReadAsStringAsync()
+
+        Assert.Equal(HttpStatusCode.NotFound, result.StatusCode)
+        Assert.Equal("text/html", result.Content.Headers.ContentType.MediaType)
+        Assert.Contains(message, content)
+    }
+
+    do! doTest "/en/blah-blah" "The requested page does not exist."
+    do! doTest "/ru/blah-blah" "Страница по этому адресу не существует."
+    do! doTest "/blah-blah" "The requested page does not exist."
 })
