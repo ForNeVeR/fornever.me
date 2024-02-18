9153b3548a210672a3decaef6bca285c10e7da95
Friedrich von Never <friedrich@fornever.me>
2024-02-18 22:21:05 +0100

    Frontend: migrate to webpack 5

--- ForneverMind.Frontend/webpack.config.js
+++ ForneverMind.Frontend/webpack.config.js
@@ -1,22 +1,28 @@
-﻿const ExtractTextPlugin = require('extract-text-webpack-plugin');
+﻿const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 const outputPath = path.join(__dirname, '..', 'ForneverMind');
 
 const client = {
-    resolve: { extensions: ['', '.js', '.less'] },
-    entry: { 'app': './style/main' },
+    resolve: {extensions: ['', '.js', '.less']},
+    entry: {'app': './style/main'},
     module: {
-        loaders: [{ test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader') }]
+        rules: [{
+            test: /\.less$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        }]
     },
-    plugins: [new ExtractTextPlugin('[name].css')],
-    output: { path: path.join(outputPath, 'wwwroot', 'app'), filename: '[name].js' }
+    plugins: [new MiniCssExtractPlugin({filename: '[name].css'})],
+    output: {path: path.join(outputPath, 'wwwroot', 'app'), filename: '[name].js'}
 };
 
 const server = {
-    resolve: { extensions: ['', '.js'] },
+    resolve: {extensions: ['', '.js']},
     entry: './server',
-    output: { path: outputPath, filename: 'server.js', libraryTarget: 'var', library: 'server' }
+    output: {path: outputPath, filename: 'server.js', libraryTarget: 'var', library: 'server'}
 };
 
 module.exports = [client, server];
