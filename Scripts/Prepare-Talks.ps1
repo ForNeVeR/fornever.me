2fea431d593150c8319d267fdae957381b326f81
Friedrich von Never <friedrich@fornever.me>
2025-09-08 19:38:33 +0200

    CI: simplify the installation script

--- Scripts/Prepare-Talks.ps1
+++ Scripts/Prepare-Talks.ps1
@@ -16,14 +16,6 @@ function log($text, $header) {
     Write-Output "[$header] $text"
 }
 
-function exec($command) {
-    log "$command $args" 'Prepare-Talks.exec'
-    & $command @args
-    if ($LASTEXITCODE -ne 0) {
-        throw "[build error] $command $args = $LASTEXITCODE"
-    }
-}
-
 function assertNotEmpty() {
     $items = Get-ChildItem .
     if (-not $items) {
@@ -37,7 +29,10 @@ function yarnInstall($name, $flags) {
     try {
         log "Installing $name"
         assertNotEmpty
-        exec $yarn install $flags
+        & $yarn install $flags
+        if ($LASTEXITCODE -ne 0) {
+            throw "[build error] $yarn install $flags = $LASTEXITCODE"
+        }
     } finally {
         Pop-Location
     }
@@ -50,7 +45,10 @@ function npmInstall($name) {
     try {
         log "Installing $name"
         assertNotEmpty
-        exec npm install
+        npm install
+        if ($LASTEXITCODE -ne 0) {
+            throw "[build error] npm install = $LASTEXITCODE"
+        }
     } finally {
         $ErrorActionPreference = 'Stop'
         Pop-Location
